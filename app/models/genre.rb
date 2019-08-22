182e188ddd915fae282ed32d7ad945229cebfed7
Tracy Reuther <tracyreuther@gmail.com>
2019-08-22 23:36:05 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.collect {|name| artists.name}
+    self.artists.collect {|name| artist.name}
   end
 end
